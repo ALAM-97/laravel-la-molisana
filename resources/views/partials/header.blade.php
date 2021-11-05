2e789ac2753a51dffe97ddd8b24ad56c482ff379
ALAM-97 <alexamara97@gmail.com>
2021-11-05 12:30:07 +0100

    Scaffolding

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="container">
+        <div class="logo">
+            <img src="{{asset("images/logo.img")}}" alt="Logo La Molisana">
+        </div>
+        <nav>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Prodotti</a></li>
+            <li><a href="#">News</a></li>
+        </nav>
+    </div>
+</header>
